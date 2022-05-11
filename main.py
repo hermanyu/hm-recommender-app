4d519307fce5fb7281da5286d6aaf17c42145bcd
unknown <hermangyu@gmail.com>
2022-05-10 18:56:38 -0700

    add exception handling to main

--- main.py
+++ main.py
@@ -15,14 +15,26 @@ pages = ["Top 10 Today", "Shop By Season", "People who bought this also bought..
 page = st.sidebar.selectbox("What kind of recommendations would you like to see?", pages)
 
 if page == pages[0]:
-    app.main()
+    try:
+        app.main()
+    except:
+        st.write('Looks like something went wrong :(')
 
 if page == pages[1]:
-    app1.main()
+    try:
+        app1.main()
+    except:
+        st.write('Looks like something went wrong :(')
 
 if page == pages[2]:
-    app2.main()
+    try:
+        app2.main()
+    except:
+        st.write('Looks like something went wrong :(')
 
 if page == pages[3]:
-    app3.main()
+    try:
+        app3.main()
+    except:
+        st.write('Looks like something went wrong :(')
 
